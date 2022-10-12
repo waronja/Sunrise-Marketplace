cb929160676e428811188a5096aee2522f2d94f7
Duncan <oluochdancun19@gmail.com>
2022-10-12 13:28:55 +0300

    generated user controller and serializer and added validations to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_205636) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_102038) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_205636) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "popularity"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
