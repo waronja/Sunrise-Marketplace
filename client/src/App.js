e9faf256e04bdcb2997e1810c7ca18dbbfa834c6
Alex Alemba <alembakiragu@gmail.com>
2022-10-12 09:25:31 +0300

    working on it

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// importing MyRouts where we located all of our theme
+import MyRouts from './routers/routes'
+
+function App() {
+  return (
+    <div>
+      <MyRouts />
+      <div>Hello</div>
+    </div>
+    
+  );
+}
+
+export default App;
