42cad59056a5066ca62e74c7dc1a38876867b262
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-10-14 00:36:11 +0300

    style footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -19,7 +19,6 @@ class Footer extends Component {
                     widgetData_1: res.data.widgetData_1,
                     widgetData_2: res.data.widgetData_2
                 })
-                // console.log(this.state.data)
             })
         .catch(err => console.log(err))
     }
@@ -33,63 +32,20 @@ class Footer extends Component {
                             <div className="col-12 col-sm-6 col-lg-3 res-margin">
                                 {/* Footer Items */}
                                 <div className="footer-items">
-                                    {/* Logo */}
-                                    {/* <a className="navbar-brand" href="/">
-                                        <img src={this.state.data.img} alt="" />
-                                    </a> */}
-                                    {/* <p>{this.state.data.content}</p> */}
-                                    {/* Social Icons */}
-                                    <div className="social-icons d-flex">
-                                        {this.state.socialData.map((item, idx) => {
-                                            return (
-                                                <a key={`sd_${idx}`} className={item.link} href="#">
-                                                    <i className={item.icon} />
-                                                    <i className={item.icon} />
-                                                </a>
-                                            );
-                                        })}
-                                    </div>
+                                    
                                 </div>
                             </div>
                             <div className="col-12 col-sm-6 col-lg-3 res-margin">
                                 {/* Footer Items */}
                                 <div className="footer-items">
-                                    {/* Footer Title */}
-                                    <h4 className="footer-title">{this.state.data.widget_1}</h4>
-                                    <ul>
-                                        {this.state.widgetData_1.map((item, idx) => {
-                                            return (
-                                                <li key={`wdo_${idx}`}><a href="#">{item.text}</a></li>
-                                            );
-                                        })}
-                                    </ul>
+                                    
                                 </div>
                             </div>
                             <div className="col-12 col-sm-6 col-lg-3 res-margin">
-                                {/* Footer Items */}
-                                <div className="footer-items">
-                                    {/* Footer Title */}
-                                    <h4 className="footer-title">{this.state.data.widget_2}</h4>
-                                    <ul>
-                                        {this.state.widgetData_2.map((item, idx) => {
-                                            return (
-                                                <li key={`wdo_${idx}`}><a href="#">{item.text}</a></li>
-                                            );
-                                        })}
-                                    </ul>
-                                </div>
+                               
                             </div>
                             <div className="col-12 col-sm-6 col-lg-3">
-                                {/* Footer Items */}
-                                <div className="footer-items">
-                                    {/* Footer Title */}
-                                    {/* <h4 className="footer-title">{this.state.data.widget_3}</h4> */}
-                                    {/* Subscribe Form */}
-                                    {/* <div className="subscribe-form d-flex align-items-center">
-                                        <input type="email" className="form-control" placeholder="info@yourmail.com" />
-                                        <button type="submit" className="btn"><i className="icon-paper-plane" /></button>
-                                    </div> */}
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
