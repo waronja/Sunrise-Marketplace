557aefd0b00304b6d01738fb6d26e74f62c42737
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-10-13 17:49:36 +0300

    style footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -83,12 +83,12 @@ class Footer extends Component {
                                 {/* Footer Items */}
                                 <div className="footer-items">
                                     {/* Footer Title */}
-                                    <h4 className="footer-title">{this.state.data.widget_3}</h4>
+                                    {/* <h4 className="footer-title">{this.state.data.widget_3}</h4> */}
                                     {/* Subscribe Form */}
-                                    <div className="subscribe-form d-flex align-items-center">
+                                    {/* <div className="subscribe-form d-flex align-items-center">
                                         <input type="email" className="form-control" placeholder="info@yourmail.com" />
                                         <button type="submit" className="btn"><i className="icon-paper-plane" /></button>
-                                    </div>
+                                    </div> */}
                                 </div>
                             </div>
                         </div>
